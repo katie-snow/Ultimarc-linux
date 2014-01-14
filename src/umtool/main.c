e737fcf905218c9a994c53db391eca330b83ec99
Katie Snow <katiej.snow@gmail.com>
2014-01-14 09:56:33 -0700

    Added code to write to the USB device for testing out the kernel code.

--- src/umtool/main.c
+++ src/umtool/main.c
@@ -8,10 +8,37 @@
  ============================================================================
  */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define DEFAULT_DEVICE "/dev/ultimarc0"
 
 int main(void) {
+	char *dev;
+	int   fd;
+	int   retVal;
+
 	puts("Nothing to do yet.");
+
+
+	dev = DEFAULT_DEVICE;
+	fd = open(dev, O_RDWR);
+
+	if (fd == -1)
+	{
+		perror("open");
+		exit(1);
+	}
+
+	retVal = write (fd, NULL, 0);
+
+	if (retVal < 0)
+	{
+		fprintf (stderr, "Error occurred\n");
+	}
+
+	close (fd);
 	return EXIT_SUCCESS;
 }
