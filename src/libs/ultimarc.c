8774733b212914ed7ac959aa9a68a7254bd0a2f5
Katie Snow <katiej.snow@gmail.com>
2016-05-05 15:39:41 -0600

    Reworked external interface.  The interface has calls to validate the configuration and calls that write the configuration to a boards.

--- src/libs/ultimarc.c
+++ src/libs/ultimarc.c
@@ -6,7 +6,7 @@
  Copyright   : Copyright 2014 Robert Abram, Katie Snow
  Description : Ultimarc main configuration library
  ============================================================================
-*/
+ */
 
 /* Unix */
 #include <linux/usbdevice_fs.h>
@@ -24,134 +24,167 @@
 #include "ipacultimate.h"
 #include "dbg.h"
 
-
-void loadUltimarcConfigurations (int argc, char **argv)
+int
+ulValidateConfig (json_object* bcfg, ulobject* ulobj)
 {
-  int idx;
-  int inner_idx;
+  int retCode = 0;
 
-  const char* fileStr = NULL;
-
-  json_object *jobj = NULL;
-  json_object *innerobj = NULL;
-  json_object *item = NULL;
-
-  for (idx = 1; idx < argc; ++idx)
+  if (bcfg)
   {
-    jobj = json_object_from_file (argv[idx]);
-    if (jobj)
+    retCode = ulGetProdAndVersion (bcfg, ulobj);
+
+    if (retCode == 0)
     {
-      log_info ("Loading %s...", argv[idx]);
-
-      if (json_object_object_get_ex(jobj, "configurations", &innerobj))
+      if ((ulobj->ipac = isIPACConfig (ulobj->pStr, ulobj->version, bcfg))
+          || (ulobj->ultimate = isIPACUltimateConfig (ulobj->pStr,
+                                                      ulobj->version, bcfg))
+          || (ulobj->pacDrive = isPACDriveConfig (ulobj->pStr, ulobj->version,
+                                                  bcfg)) || (ulobj->pacLED =
+              isPACLED64Config (ulobj->pStr, ulobj->version, bcfg))
+          || (ulobj->ultistik = isUltistikConfig (ulobj->pStr, ulobj->version,
+                                                  bcfg)))
       {
-        printf ("\n");
-        for (inner_idx = 0; inner_idx < json_object_array_length(innerobj); ++ inner_idx)
-        {
-          item = json_object_array_get_idx(innerobj, inner_idx);
-          fileStr = json_object_get_string(item);
-          item = json_object_from_file (fileStr);
-
-          if (item)
-          {
-            log_info ("Loading %s...", fileStr);
-            updateUltimarcBoard(item);
-          }
-          else
-          {
-            log_err ("%s. Format invalid\n", fileStr);
-          }
-        }
+        log_info("Configuration is %s. [Validated]", ulobj->pStr);
       }
       else
       {
-        updateUltimarcBoard(jobj);
+        log_err("Configuration is '%s'. [Not validated].", ulobj->pStr);
       }
     }
     else
     {
-      log_err ("%s. Format invalid\n", argv[idx]);
+      log_err("Configuration is '%s'. [Not validated].", ulobj->pStr);
     }
   }
+  else
+  {
+    retCode = -1;
+    log_err("JSON format invalid");
+  }
+
+  return retCode;
 }
 
-bool updateUltimarcBoard (json_object* jobj)
+int
+ulValidateConfigFileStr (const char* file, ulobject* ulobj)
 {
-  bool ret = false;
+  json_object *bcfg = json_object_from_file (file);
 
-  const char* prodstr = NULL;
-  int version = 0;
+  return ulValidateConfig (bcfg, ulobj);
+}
+
+int
+ulWriteToBoard (json_object* bcfg, ulobject* ulobj)
+{
+  int retCode = 0;
+
+  if (bcfg && ulobj)
+  {
+    if (ulobj->ipac)
+    {
+      retCode = updateBoardIPAC (bcfg);
+    }
+    else if (ulobj->ultimate)
+    {
+      log_info("Updating IPAC Ultimate board...");
+      retCode = updateBoardIPacUltimate (bcfg);
+    }
+    else if (ulobj->pacDrive)
+    {
+      log_info("Updating PAC Drive board...");
+      retCode = updateBoardPacDrive (bcfg);
+    }
+    else if (ulobj->pacLED)
+    {
+      log_info("Updating PAC LED 64 board...");
+      retCode = updateBoardPacLED (bcfg);
+    }
+    else if (ulobj->ultistik)
+    {
+      log_info("Updating Ultistik board...");
+      retCode = updateBoardULTISTIK (bcfg);
+    }
+
+    if (retCode)
+    {
+      log_info("Board update successful.");
+    }
+    else
+    {
+      log_info("Board update failed.");
+    }
+  }
+  else
+  {
+    retCode = -1;
+    log_info("Board update failed.");
+  }
+
+  return retCode;
+}
+
+int
+ulWriteToBoardFileStr (const char* file, ulobject* ulobj)
+{
+  json_object *bcfg = json_object_from_file (file);
+
+  return ulWriteToBoard (bcfg, ulobj);
+}
+
+int
+ulGetProdAndVersion (json_object* jobj, ulobject* ulobj)
+{
+  int retCode = 0;
 
   json_object* prodobj = NULL;
   json_object* verobj = NULL;
 
-  if (json_object_object_get_ex(jobj, "product", &prodobj) &&
-      json_object_get_type(prodobj) == json_type_string)
+  if (jobj && ulobj)
   {
-    if (json_object_object_get_ex(jobj, "version", &verobj) &&
-        json_object_get_type(verobj) == json_type_int)
+    if (json_object_object_get_ex (jobj, "product", &prodobj))
     {
-      prodstr = json_object_get_string(prodobj);
-      version = json_object_get_int(verobj);
-
-      if (isIPACConfig(prodstr, version, jobj))
+      if (json_object_get_type (prodobj) == json_type_string)
       {
-        ret = updateBoardIPAC(jobj);
-      }
-      else if (isIPACUltimateConfig(prodstr, version, jobj))
-      {
-        log_info ("Updating IPAC Ultimate board...");
-        ret = updateBoardIPacUltimate(jobj);
-      }
-      else if (isPACDriveConfig(prodstr, version, jobj))
-      {
-        log_info ("Updating PAC Drive board...");
-        ret = updateBoardPacDrive(jobj);
-      }
-      else if (isPACLED64Config(prodstr, version, jobj))
-      {
-        log_info ("Updating PAC LED 64 board...");
-        ret = updateBoardPacLED(jobj);
-      }
-      else if (isUltistikConfig(prodstr, version, jobj))
-      {
-        log_info ("Updating Ultistik board...");
-        ret = updateBoardULTISTIK(jobj);
-      }
-    }
-    else
-    {
-      if (json_object_object_get_ex(jobj, "version", &verobj))
-      {
-        log_err ("'version' is not defined as a integer");
+        ulobj->pStr = json_object_get_string (prodobj);
       }
       else
       {
-        log_err ("'version' is not defined in the configuration file");
+        log_err("'product' is not defined as a string");
+        ulobj->pStr = "";
+        retCode = 1;
       }
-    }
-  }
-  else
-  {
-    if (json_object_object_get_ex(jobj, "product", &prodobj))
-    {
-      log_err ("'product' is not defined as a string");
     }
     else
     {
-      log_err ("'product' is not defined in the configuration file");
+      log_err("'product' is not defined in the configuration file");
+      ulobj->pStr = "";
+      retCode = 1;
     }
-  }
 
-  if (ret)
-  {
-    log_info ("Update done.\n");
+    if (json_object_object_get_ex (jobj, "version", &verobj))
+    {
+      if (json_object_get_type (verobj) == json_type_int)
+      {
+        ulobj->version = json_object_get_int (verobj);
+      }
+      else
+      {
+        log_err("'version' is not defined as a integer");
+        ulobj->version = -1;
+        retCode = 1;
+      }
+    }
+    else
+    {
+      log_err("'version' is not defined in the configuration file");
+      ulobj->version = -1;
+      retCode = 1;
+    }
   }
   else
   {
-    log_info ("Update failed!\n");
+    log_err("JSON_Object is null");
   }
 
-  json_object_put(jobj);
-  return ret;
+  return retCode;
 }
--- src/libs/ultimarc.h
+++ src/libs/ultimarc.h
@@ -6,30 +6,55 @@
  Copyright   : Copyright 2014 Robert Abram, Katie Snow
  Description : Ultimarc main configuration library
  ============================================================================
-*/
+ */
 
 #ifndef __ULTIMARC_H_
 #define __ULTIMARC_H_
-
 
 /* Unix */
 #include <stdbool.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 typedef struct json_object json_object;
 
-extern void loadUltimarcConfigurations(int argc, char **argv);
+typedef struct ulobject
+{
+  const char* pStr;
+  int version;
+  bool ipac;
+  bool ultimate;
+  bool pacDrive;
+  bool pacLED;
+  bool ultistik;
+} ulobject;
 
-/*
- * updateUltimarcBoard
- * Updates a board.  The board is selected based on the json file contents
- * Paramter: json object
- * Returns: true if successfully updated a board, false otherwise
+/**
+ * ulValidateConfig
+ * Validates the configuration
  */
-bool updateUltimarcBoard(json_object* jobj);
+extern int
+ulValidateConfig (json_object* bcfg, ulobject* ulobj);
+extern int
+ulValidateConfigFileStr (const char* file, ulobject* ulobj);
+
+/**
+ * ulWriteToBoard
+ * Writes the configuration provided out to the Ultimarc hardware
+ */
+extern int
+ulWriteToBoard (json_object* bcfg, ulobject* ulobj);
+extern int
+ulWriteToBoardFileStr (const char* file, ulobject* ulobj);
+
+/**
+ * Populates the ulobj with the product and version in the json_object
+ */
+int
+ulGetProdAndVersion (json_object* jobj, ulobject* ulobj);
 
 #ifdef __cplusplus
 }
