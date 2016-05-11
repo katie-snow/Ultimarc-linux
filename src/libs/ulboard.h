80dbd051e6a8d3b2976b9adc7f0254d6cc0eabcc
Katie Snow <katiej.snow@gmail.com>
2016-05-10 22:21:50 -0600

    Add new file to library.  A structure for holding board information that is common between all the boards.

---
+++ src/libs/ulboard.h
@@ -0,0 +1,59 @@
+/*
+ ============================================================================
+ Name        : ulboard.h
+ Author      : Katie Snow
+ Version     :
+ Copyright   : Copyright 2014 Robert Abram, Katie Snow
+ Description : Ultimarc board types and structures
+ ============================================================================
+ */
+
+#ifndef ULTIMARCBOARD_H_
+#define ULTIMARCBOARD_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* All the supported board types */
+typedef enum ulboard_type
+{
+  ulboard_type_null,
+  ulboard_type_ipac2,
+  ulboard_type_ipac4,
+  ulboard_type_jpac,
+  ulboard_type_minipac,
+  ulboard_type_ultimate,
+  ulboard_type_pacDrive,
+  ulboard_type_pacLED,
+  ulboard_type_ultistik
+} ulboard_type;
+
+/* Supported versions */
+typedef enum ulboard_version
+{
+  ulboard_version_null,
+  ulboard_version_pre2015,
+  ulboard_version_2015
+} ulboard_version;
+
+/* Data structure */
+typedef struct ulboard
+{
+  ulboard_type type;
+  ulboard_version version;
+} ulboard;
+
+extern const char* ulBoardTypeToString (ulboard_type bType);
+
+extern ulboard_type ulStringToBoardType (const char* bStr);
+
+extern ulboard_version ulIntToBoardVersion (int bVersion);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* ULTIMARCBOARD_H_ */
